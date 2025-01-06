40fff1fa1e1cc5b73b9f4b1ad61627880e289166
Hassanhamidi6 <hassanhamidi612@gmail.com>
2025-01-06 17:55:52 +0500

    Dictionary Basics

--- DS/dict/dict1.py
+++ DS/dict/dict1.py
@@ -7,25 +7,32 @@ key value pairs
 m={}
 print(type(m))
 
-#initializing dict
 
+#initializing dict
 k={
     1:"arham",
     2:"hassan"
 }
 
-# print(k["a"])
+#accessing elements in dict
+print(k[1])
 
+
+#dictionary eng-urdu
 meaning={
     "what":"kia",
     "why":"kyun",
     "who":"kon"
 }
-
 string=input("Enter the word i will tell you the meaning")
 print(meaning[string])
 
-meaning["what"]="kab"
+#adding new element in dict
+meaning["when"]="kab"
 print(meaning)
-print(dir(dict))
+
+
+#updating new element in dict
+meaning["what"]="kia hua"
+print(meaning)
 
