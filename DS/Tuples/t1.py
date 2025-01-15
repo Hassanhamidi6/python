24311d725a7ffc028c22aab3f453ec6b3aacd543
Hassanhamidi6 <hassanhamidi612@gmail.com>
2025-01-15 23:54:15 +0500

    Tuples

--- DS/Tuples/t1.py
+++ DS/Tuples/t1.py
@@ -1,2 +1,28 @@
+#defination
 m=(1,2)
+
+#accessing elements
+print(m[0])
+
+#type
 print(type(m))
+
+#methods
+print(dir(tuple))
+
+print(m.count(1))
+print(m.index(2))
+
+#functions
+print(sum(m))
+print(min(m))
+print(max(m))
+print(len(m))
+#aggregate functions
+#if else with tuple
+#loop with tuples
+for i in m:
+    if i >1:
+        print("greater")
+    else:
+        print("lesser")
--- DS/set/s1.py
+++ DS/set/s1.py
@@ -1,7 +1,7 @@
 '''
 sets
 '''
-m={1,4,2,3,4,5}
+m={1,4,3,4,5}
 print(m)
 
 print(dir(set))
@@ -17,6 +17,20 @@ print (k.difference(n,m))
 
 
 m.discard(4)
-print (m)       
+print (m)    
+
+#loop with sets
+for i in m:
+    print(i) 
+
+l=[1,1,1,2,2,2,3,3,3]
+
+l=list(set(l))
+
+#sets are not subscriptable, not allow slicing
+print(sum(m))
+print(min(m))
+print(max(m))
+print(len(m))
 
 
