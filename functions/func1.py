595e280e3a7c1d9593048c9cc2b825973b9baf62
Hassanhamidi6 <hassanhamidi612@gmail.com>
2025-01-17 00:07:54 +0500

    Func

---
+++ functions/func1.py
@@ -0,0 +1,17 @@
+'''
+Function: set of instructions which perform particular task
+Function: input ----> [Function] -----> Output
+Advantages: Code Reusability
+Functional Programming
+'''
+
+#camel case writing
+
+#function define
+
+def Add(a,b):
+    c=a+b
+    
+a=Add(4,5)
+print(a)
+
