bbccf86b9a9297335cc74f6f4ad85f7e054d153a
Hassanhamidi6 <hassanhamidi612@gmail.com>
2025-03-08 21:24:54 +0500

    pandasp1.py

--- pandas/p1.py
+++ pandas/p1.py
@@ -3,6 +3,7 @@ Pandas
 Pandas is a package in python 
 
 Data frame --> in Pandas use to represent data with rows and columns (tabular data )
+Pandas handle only managed datas
 '''
 
 import pandas as pd 
